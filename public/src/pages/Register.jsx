98446000d8f55dd89ed6bceb778b17117c8a1d29
AnicetFantomas <anicetmurhula20@gmail.com>
2023-06-13 12:08:13 +0200

    Update register form

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,53 +1,103 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import Logo from "../assets/logo.svg";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+import { registerRoute } from "../utils/APIRoutes";
 
 const Register = () => {
-  const handleSubmit = (e) => {
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const toastOptions = {
+    position: "bottom-right",
+    autoClose: 8000,
+    pauseOnHover: true,
+    draggable: true,
+    theme: "dark",
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    alert("form");
+    if (handleValidation()) {
+        const { username, email, password, confirmPassword } = values;
+        const {data} = await axios.post(registerRoute, {
+          username,
+          email,
+          password,
+        })
+    }
+  };
+
+  const handleValidation = () => {
+    const { username, email, password, confirmPassword } = values;
+    if (password !== confirmPassword) {
+      toast.error("Passwords do not match", toastOptions);
+      return false;
+    } else if (username.length < 3){
+        toast.error("Username should be greater than 3 characters", toastOptions);
+        return false;
+    } else if (password.length < 8){
+        toast.error("password should be equal or greater than 8 characters", toastOptions);
+        return false;
+    } else if (email === "") {
+      toast.error("Email is required", toastOptions);
+      return false;
+    }
+    return true;
   };
 
   const handleChange = (e) => {
-
+    setValues({ ...values, [e.target.name]: e.target.value });
+    // console.log(values);
   };
 
   return (
-    <FormContainer>
-      <form onSubmit={(e) => handleSubmit(e)}>
-        <div className="brand">
-          <img src={Logo} alt="logo" />
-          <h1>snappy</h1>
-        </div>
-        <input
-          type="text"
-          placeholder="username"
-          name="username"
-          onChange={(e) => handleChange(e)}
-        />
-        <input
-          type="email"
-          placeholder="email"
-          name="email"
-          onChange={(e) => handleChange(e)}
-        />
-        <input
-          type="password"
-          placeholder="password"
-          name="password"
-          onChange={(e) => handleChange(e)}
-        />
-        <input
-          type="password"
-          placeholder="confirm password"
-          name="confrmPassword"
-          onChange={(e) => handleChange(e)}
-        />
-        <button type="submit">Create User</button>
-        <span>Already have an account? <Link to="/login">Login</Link></span>
-      </form>
-    </FormContainer>
+    <>
+      <FormContainer>
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <div className="brand">
+            <img src={Logo} alt="logo" />
+            <h1>snappy</h1>
+          </div>
+          <input
+            type="text"
+            placeholder="username"
+            name="username"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="email"
+            placeholder="email"
+            name="email"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            name="password"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="password"
+            placeholder="confirm password"
+            name="confrmPassword"
+            onChange={(e) => handleChange(e)}
+          />
+          <button type="submit">Create User</button>
+          <span>
+            Already have an account? <Link to="/login">Login</Link>
+          </span>
+        </form>
+      </FormContainer>
+      <ToastContainer />
+    </>
   );
 };
 const FormContainer = styled.div`
@@ -60,14 +110,63 @@ const FormContainer = styled.div`
   gap: 1rem;
   background-color: #131324;
   .brand {
-      display: flex;
-      align-items: center;
-      gap: 1rem;
-      justify-content: center;
-      img {
-          height: 5rem;
-      }
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    justify-content: center;
+    img {
+      height: 5rem;
+    }
+    h1 {
+      color: white;
+      text-transform: uppercase;
+    }
   }
-  `;
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    background-color: #00000076;
+    border-radius: 2rem;
+    padding: 3rem 5rem;
+    input {
+      background-color: transparent;
+      padding: 1rem;
+      border: 0.1rem solid #4e0eff;
+      border-radius: 0.4rem;
+      color: white;
+      width: 100%;
+      font-size: 1rem;
+      &:focus {
+        border: 0.1rem solid #997af0;
+        outline: none;
+      }
+    }
+    button {
+      background-color: #997af0;
+      color: white;
+      padding: 1rem 2rem;
+      border: none;
+      font-weight: bold;
+      cursor: pointer;
+      border-radius: 0.4rem;
+      font-size: 1rem;
+      text-transform: uppercase;
+      transition: 0.5s ease-in-out;
+      &:hover {
+        background-color: #4e0eff;
+      }
+    }
+    span {
+      color: white;
+      text-transform: uppercase;
+      a {
+        color: #4e0eff;
+        text-decoration: none;
+        font-weight: bold;
+      }
+    }
+  }
+`;
 
 export default Register;
